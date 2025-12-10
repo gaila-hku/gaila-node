848ea4c3ddb2dfd4c64b91e60571e366e7c32f96
Oscar Fong <hyfongoscar@gmail.com>
2025-12-10 23:55:14 +0800

    feat(home): version api

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -6,6 +6,9 @@ const router = Router();
 router.get('/health', (req, res) => {
   res.sendStatus(200);
 });
+router.get('/version', (req, res) => {
+  res.json({ version: '0.0.1' });
+});
 
 router.post('/refresh-categories', refreshPromptCategories);
 
